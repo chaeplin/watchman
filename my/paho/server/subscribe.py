e34e7232b986666c0174d1d59578d24201d36de0
chaeplin <chaeplin@gmail.com>
2017-09-06 02:36:59 +0900

    deque and ignore retained msg

--- my/paho/server/subscribe.py
+++ my/paho/server/subscribe.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # /etc/rc.local 
-# sleep 60 && su - pi -c "screen -dm -S mqtt /home/pi/mqtt/s.py"
+# sleep 60 && su - pi -c "screen -dm -S mqtt /home/pi/mqtt/subscribe.py"
 
 import io, os, sys
 import time
@@ -11,68 +11,80 @@ import json
 import requests
 import paho.mqtt.client as mqtt
 import threading
+from collections import deque
 
 webhook_url = 'https://hooks.slack.com/services/xxx/xxx/xxx'
 
 class threading_sendslack(threading.Thread):
 
-    def __init__(self, msgtoslack, evtime):
+    def __init__(self):
         threading.Thread.__init__(self)
-        self.msgtoslack = msgtoslack
-        self.evtime = evtime
 
     def run(self):
-        slack_data = { "text": self.msgtoslack + ' - ' + str(self.evtime) }
-        print(slack_data)
-        try:
-            response = requests.post(webhook_url, data=json.dumps(slack_data), headers={'Content-Type': 'application/json'})
-            if response.status_code != 200:
-                print('slack post failed')
+        while True:
+            if len(q) > 0:
+                mm = '\n'.join(q)
+                q.clear()
 
-        except:
-            pass
+                slack_data = { "text": mm }
 
-def start_slackpost(msgtoslack, evtime):
-    spost = threading_sendslack(msgtoslack, evtime)
+                try:
+                    response = requests.post(webhook_url, data=json.dumps(slack_data), headers={'Content-Type': 'application/json'})
+                    if response.status_code != 200:
+                        print('slack post failed')
+
+                except:
+                    pass
+
+                time.sleep(1)
+
+def start_slackpost():
+    spost = threading_sendslack()
     spost.start()
-
-
-def sendslack(msgtoslack, evtime):
-    slack_data = { "text": msgtoslack + ' - ' + str(evtime) }
-
-    print(slack_data)
-    response = requests.post(webhook_url, data=json.dumps(slack_data), headers={'Content-Type': 'application/json'})
-    if response.status_code != 200:
-         raise ValueError('Request to slack returned an error %s, the response is:\n%s' % (response.status_code, response.text))
 
 def on_connect(client, userdata, flags, rc):
     print ("Connected with result code "+str(rc))
     client.subscribe(subscribetopic, qos=0)
-#    client.subscribe("$SYS/#", qos=0)
 
 def on_message(client, userdata, msg):
-#    print (msg.topic+" "+str(msg.payload))
-#    result = json.loads(msg.payload.decode("utf-8"))
-
     print(now(), msg.topic, msg.payload)
     try:   
         result = json.loads(msg.payload.decode("utf-8"))
-    #   print(json.dumps(result, sort_keys=True, indent=4, separators=(',', ': ')))
 
-    #    use redis to store last status of client to filter out same msg
         host_status = result.get('msg', None)
+        host_name   = result.get('hostname', None)
+
+        slack_msg = host_name + ' : ' + host_status + ' - ' + str(now())
+        if now() - start_time < 3:
+            return
+
         if host_status == 'down' or host_status == 'up':
-            start_slackpost((result.get('hostname') + ' is ' + host_status), now())
-            #sendslack((result.get('hostname') + ' is ' + host_status), now())
+            if slack_msg_list.get(host_name, None) != None:
+                old_status = slack_msg_list.get(host_name).get('host_status', None)
+                if old_status != host_status:
+                    slack_msg_list[host_name] = { 'host_status': host_status }
+                    q.append(slack_msg)
+
+            else:
+                slack_msg_list[host_name] = { 'host_status': host_status }
+                q.append(slack_msg)
 
     except:
         pass
+
 
 def now():
     return int(time.time())
 
 
 if __name__ == "__main__":
+
+
+    q  = deque(maxlen=100)
+    slack_msg_list = {}
+    start_time = now()
+
+    start_slackpost()
 
     hostname = socket.gethostname()
 
@@ -97,4 +109,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         client.disconnect()
         sys.exit(1)
-
