3acda5b4c079bbaf125c01fb2a9b9a2302782663
chaeplin <chaeplin@gmail.com>
2017-09-06 17:15:15 +0900

    2 deque. formatting slack msg

--- my/paho/server/subscribe.py
+++ my/paho/server/subscribe.py
@@ -22,21 +22,57 @@ class threading_sendslack(threading.Thread):
 
     def run(self):
         while True:
-            if len(q) > 0:
-                mm = '\n'.join(q)
-                q.clear()
-
-                slack_data = { "text": mm }
+            deque_down_len = len(qdown)
+            deque_up_len = len(qup)
+            if deque_down_len > 0:
+                deque_down_list = [qdown.popleft() for _i in range(deque_down_len)]
+                slack_down_data = {
+                    'attachments': [
+                        {
+                            'pretext': 'device down report',
+                            'title': str(deque_down_len) + ' is down',
+                            'color': 'danger',
+                            'fields': deque_down_list,
+                            'ts': now()
+                        }
+                    ]
+                }
 
                 try:
-                    response = requests.post(webhook_url, data=json.dumps(slack_data), headers={'Content-Type': 'application/json'})
+                    print(slack_down_data)
+                    response = requests.post(webhook_url, data=json.dumps(slack_down_data), headers={'Content-Type': 'application/json'})
                     if response.status_code != 200:
                         print('slack post failed')
 
                 except:
                     pass
 
-                time.sleep(1)
+
+            if deque_up_len > 0:
+                deque_up_list = [qup.popleft() for _i in range(deque_up_len)]
+                slack_up_data = {
+                    'attachments': [
+                        {
+                            'pretext': 'device up report',
+                            'title': str(deque_up_len) + ' is up',
+                            'color': 'good',
+                            'fields': deque_up_list,
+                            'ts': now()
+                        }
+                    ]
+                }
+
+                try:
+                    print(slack_up_data)
+                    response = requests.post(webhook_url, data=json.dumps(slack_up_data), headers={'Content-Type': 'application/json'})
+                    if response.status_code != 200:
+                        print('slack post failed')
+
+                except:
+                    pass
+
+
+            time.sleep(1)
 
 def start_slackpost():
     spost = threading_sendslack()
@@ -51,23 +87,41 @@ def on_message(client, userdata, msg):
     try:   
         result = json.loads(msg.payload.decode("utf-8"))
 
-        host_status = result.get('msg', None)
-        host_name   = result.get('hostname', None)
+        host_status    = result.get('msg', None)
+        host_name      = result.get('hostname', None)
+        host_timestamp = result.get('timestamp', None)
 
-        slack_msg = host_name + ' : ' + host_status + ' - ' + str(now())
-        if now() - start_time < 3:
-            return
+        if host_status == 'up':
+            status_emoji = ':white_check_mark:'
+        elif host_status == 'down':
+            status_emoji = ':o:'
+        else:
+            status_emoji = ':grey_question:'
+
+        slack_msg = {
+                'title': status_emoji + ' ' + host_name 
+        }
+
+#        if now() - start_time < 3:
+#            return
 
         if host_status == 'down' or host_status == 'up':
             if slack_msg_list.get(host_name, None) != None:
                 old_status = slack_msg_list.get(host_name).get('host_status', None)
                 if old_status != host_status:
                     slack_msg_list[host_name] = { 'host_status': host_status }
-                    q.append(slack_msg)
+                    if host_status == 'down':
+                        qdown.append(slack_msg)
+                    else:
+                        qup.append(slack_msg)
+                        
 
             else:
                 slack_msg_list[host_name] = { 'host_status': host_status }
-                q.append(slack_msg)
+                if host_status == 'down':
+                    qdown.append(slack_msg)
+                else:
+                    qup.append(slack_msg)
 
     except:
         pass
@@ -80,7 +134,9 @@ def now():
 if __name__ == "__main__":
 
 
-    q  = deque(maxlen=100)
+    qup   = deque(maxlen=100)
+    qdown = deque(maxlen=100)
+ 
     slack_msg_list = {}
     start_time = now()
 
@@ -109,3 +165,5 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         client.disconnect()
         sys.exit(1)
+
+
